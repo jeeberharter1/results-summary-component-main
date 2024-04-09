ec68a0684c49249fc288217a4ac8feef8162703d
jeeberharter1 <jeeberharter1@gmail.com>
2024-04-08 19:13:43 -0500

    fixed script and css tag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// Retrieve data
-let response = fetch('../data.json');
-let data = response.json();
-
 // Variables
 const scores = document.querySelectorAll('#score');
 const averageScore = document.getElementById('average-score');
@@ -9,17 +5,19 @@ const categories = document.querySelectorAll('.category');
 
 let totalScore = 0;
 
-// Set scores
-Array.from(scores).map((score, index) => {
-  totalScore += data[index].score;
-  score.innerHTML = data[index].score;
-})
+fetch('../data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    Array.from(scores).map((score, index) => {
+      totalScore += data[index].score;
+      score.innerHTML = data[index].score;
+    })
+    
+    Array.from(categories).map((category, index) => {
+      console.log(category);
+      category.firstElementChild.src = data[index].icon;
+      category.lastElementChild.innerHTML = data[index].category;
+    })
 
-averageScore.innerHTML = Math.floor(totalScore / scores.length);
-
-// Set category data
-Array.from(categories).map((category, index) => {
-  console.log(category);
-  category.firstElementChild.src = data[index].icon;
-  category.lastElementChild.innerHTML = data[index].category;
-})
+    averageScore.innerHTML = Math.floor(totalScore / scores.length);
+  })

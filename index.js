8d6d4fd6784b53d622b386d60015ba8615885e8b
jeeberharter1 <jeeberharter1@gmail.com>
2024-04-08 19:27:41 -0500

    fixed script and css tag

--- index.js
+++ index.js
@@ -5,19 +5,23 @@ const categories = document.querySelectorAll('.category');
 
 let totalScore = 0;
 
-fetch('../data.json')
+// Retrieve json data
+fetch('data.json')
   .then((res) => res.json())
   .then((data) => {
+    // Set scores
     Array.from(scores).map((score, index) => {
       totalScore += data[index].score;
       score.innerHTML = data[index].score;
     })
     
+    // Set category data
     Array.from(categories).map((category, index) => {
       console.log(category);
       category.firstElementChild.src = data[index].icon;
       category.lastElementChild.innerHTML = data[index].category;
     })
 
+    // Set average score
     averageScore.innerHTML = Math.floor(totalScore / scores.length);
   })
